352276e1b59d9ccaf6c30ef192dcd6713100c265
Maddox-RVS <rynvnskl@yahoo.com>
2025-06-08 17:18:32 -0400

    Added new test trading strategy.

---
+++ tests/my_test_strategies.py
@@ -0,0 +1,23 @@
+from .. import stratify
+import random
+
+class MyTestStrategy_BuyAndHold(stratify.Strategy):
+    def __init__(self):
+        super().__init__()
+
+        self.bought: bool = False
+
+    def next(self):
+        if not self.bought: self.buy(100)
+        self.bought = True
+
+class MyTestStrategy_Random(stratify.Strategy):
+    def __init__(self):
+        super().__init__()
+
+    def next(self):
+        buyOrSell: int = random.randint(0, 1)
+        if buyOrSell == 0:
+            self.buy(units=20)
+        else:
+            self.sell(units=20)
