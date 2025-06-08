9c4fbe9ad6ac95c4521bf35b88a946bef30c4430
Maddox-RVS <rynvnskl@yahoo.com>
2025-06-07 22:10:37 -0400

    Made small change to FinalPortfolioTracker class.

--- stratify/stats/trackers/final_portfolio_value.py
+++ stratify/stats/trackers/final_portfolio_value.py
@@ -1,10 +1,11 @@
 from ..statistic_tracker import StatisticTracker
+from typing import Union
 
 class FinalPortfolioValueTracker(StatisticTracker):
     def __init__(self):
         super().__init__('final_portfolio_value')
 
-        self.finalValue: float = 0.0
+        self.finalValue: Union[None, float] = None
 
     def end(self) -> None:
         self.finalValue = self.portfolioValue
